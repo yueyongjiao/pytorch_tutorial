516d534c8287d838370cd28eeafc94460803be1c
yueyongjiao <yueyongjiao@ruc.edu.cn>
2018-09-10 18:58:40 +0800

    fda

--- LeNet.py
+++ LeNet.py
@@ -1,18 +1,23 @@
 #-*-coding:utf-8-*-
-import torch as t
+
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class Net(nn.Module):
 	def __init__(self):
-		super(Net,self).__init__()
-		#卷积层
+		# nn.Module子类的函数必须在构造函数中执行父类的构造函数
+		# 下式等价于nn.Module.__init__(self)
+		super(Net, self).__init__()
+
+		# 卷积层 '1'表示输入图片为单通道, '6'表示输出通道数，'5'表示卷积核为5*5
 		self.conv1 = nn.Conv2d(1, 6, 5)
-		self.conv1 = nn.Conv2d(6, 16, 5)
-		#全连接层
+		# 卷积层
+		self.conv2 = nn.Conv2d(6, 16, 5)
+		# 仿射层/全连接层，y = Wx + b
 		self.fc1 = nn.Linear(16 * 5 * 5, 120)
-		self.fc2 = nn.Linear(120,84)
-		self.fc3 = nn.Linear(84,10)
+		self.fc2 = nn.Linear(120, 84)
+		self.fc3 = nn.Linear(84, 10)
 
 	def forward(self, x):
 		# 卷积 -> 激活 -> 池化
@@ -24,18 +29,7 @@ class Net(nn.Module):
 		x = F.relu(self.fc2(x))
 		x = self.fc3(x)
 		return x
+
+
 net = Net()
 print(net)
-
-params = list(net.parameters())
-print(len(params))
-
-for name,parameters in net.named_parameters():
-    print(name,':',parameters.size())
-
-test = t.randn(1, 1, 4, 4)
-print("test: ", test)
-input = t.randn(1, 1, 32, 32)
-out = net(input)
-print(out.size())
-
