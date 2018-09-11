793f0d39e206c62d8d72393ff05f5cfb90ba0198
yueyongjiao <yueyongjiao@ruc.edu.cn>
2018-09-11 10:51:42 +0800

    dsa

--- data_load_process.py
+++ data_load_process.py
@@ -85,3 +85,64 @@ class Net(nn.Module):
 
 net = Net()
 print(net)
+
+
+from torch import optim
+criterion = nn.CrossEntropyLoss() # 交叉熵损失函数
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+t.set_num_threads(8)
+for epoch in range(2):
+    running_loss = 0.0
+    for i,data in enumerate(trainloader,0):
+        # 输入数据
+        inputs, labels = data
+
+        # 梯度清零
+        optimizer.zero_grad()
+
+        # forward + backward
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+
+        # 更新参数
+        optimizer.step()
+
+        # 打印log信息
+        # loss 是一个scalar,需要使用loss.item()来获取数值，不能使用loss[0]
+        running_loss += loss.item()
+        if i % 2000 == 1999:  # 每2000个batch打印一下训练状态
+            print('[%d, %5d] loss: %.3f' \
+                  % (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+    print('Finished Training')
+
+
+dataiter = iter(testloader)
+images, labels = dataiter.next() # 一个batch返回4张图片
+print('实际的label: ', ' '.join(\
+            '%08s'%classes[labels[j]] for j in range(4)))
+show(tv.utils.make_grid(images / 2 - 0.5)).resize((400,100))
+# 计算图片在每个类别上的分数
+outputs = net(images)
+# 得分最高的那个类
+_, predicted = t.max(outputs.data, 1)
+
+print('预测结果: ', ' '.join('%5s'\
+            % classes[predicted[j]] for j in range(4)))
+
+correct = 0 # 预测正确的图片数
+total = 0 # 总共的图片数
+
+
+# 由于测试的时候不需要求导，可以暂时关闭autograd，提高速度，节约内存
+with t.no_grad():
+    for data in testloader:
+        images, labels = data
+        outputs = net(images)
+        _, predicted = t.max(outputs, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum()
+
+print('10000张测试集中的准确率为: %d %%' % (100 * correct / total))
