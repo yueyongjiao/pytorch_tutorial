5dae3442ae9b13cbb9a7896ff621039492878977
yueyongjiao <yueyongjiao@ruc.edu.cn>
2018-09-18 09:58:59 +0800

    chapter4 nn

--- chapter4/nn.py
+++ chapter4/nn.py
@@ -4,14 +4,14 @@ from torch import nn
 from torch.autograd import Variable as V
 import numpy as np
 
-class Linear(nn.Module):
-    def __init__(self, in_feature, out_feature):
-        super(Linear, self).__init__()
-        self.w = nn.Parameter(t.randn(in_feature, out_feature))
-        self.b = nn.Parameter(t.randn(in_feature, out_feature))
+class Linear(nn.Module): # 继承nn.Module
+    def __init__(self, in_features, out_features):
+        super(Linear, self).__init__() # 等价于nn.Module.__init__(self)
+        self.w = nn.Parameter(t.randn(in_features, out_features))
+        self.b = nn.Parameter(t.randn(out_features))
 
     def forward(self, x):
-        x = x.mm(self.w)
+        x = x.mm(self.w)  # x.@(self.w)
         return x + self.b.expand_as(x)
 
 
@@ -20,14 +20,14 @@ input = V(t.randn(2, 4))
 output = layer(input)
 print(output)
 
-
-for name, parameter in layer.named_parameters():
-    print(name, parameter)
-
-a = np.array([[1, 2], [3, 4], [5, 6]])
-b = np.array([[1, 2], [3, 4]])
-a1 = V(t.from_numpy(a))
-b1 = V(t.from_numpy(b))
-
-y = a1.mm(b1)
-print(y)
+#
+# for name, parameter in layer.named_parameters():
+#     print(name, parameter)
+#
+# a = np.array([[1, 2], [3, 4], [5, 6]])
+# b = np.array([[1, 2], [3, 4]])
+# a1 = V(t.from_numpy(a))
+# b1 = V(t.from_numpy(b))
+#
+# y = a1.mm(b1)
+# print(y)
